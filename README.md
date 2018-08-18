c0cc20684c3a5b4eb6d430cf3416d32072581aef
Diego Pontoriero <dpontor@gmail.com>
2018-08-18 11:51:46 -0700

    Commit program, readme, gitignore.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # pj
-Command line tool to pretty-print JSON
+
+`pj` is a simple command line tool that reads JSON from stdin, round-trips it
+through the decoder, and pretty-prints it to stdout.
+
+## Installation
+
+`go get -u github.com/diegs/pj`
+
+## Usage
+
+`pj < input.json > output.json`
+
+or
+
+`cat input.json | pj`
+
+## Why not just use jq?
+
+`jq` does not handle numbers larger than 2^53, which truncates 64-bit integers.
---
+++ pj.go
@@ -0,0 +1,33 @@
+// Binary pj reads JSON from stdin, round-trips it through the decoder, and pretty-prints it to stdout.
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+func roundtripJSON(in, out *os.File) error {
+	raw := make(map[string]interface{})
+	d := json.NewDecoder(bufio.NewReader(in))
+	d.UseNumber()
+	if err := d.Decode(&raw); err != nil {
+		return fmt.Errorf("read json: %v", err)
+	}
+
+	e := json.NewEncoder(bufio.NewWriter(out))
+	e.SetIndent("", "  ")
+	if err := e.Encode(&raw); err != nil {
+		return fmt.Errorf("write json: %v", err)
+	}
+
+	return nil
+}
+
+func main() {
+	if err := roundtripJSON(os.Stdin, os.Stdout); err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+}
